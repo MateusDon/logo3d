a639d606a17cbe98ce151aa10ecb49e5e3345cf8
MateusDon <canaldoinsolente@gmail.com>
2026-05-30 14:46:09 -0300

    Add files via upload

---
+++ senhas.js
@@ -0,0 +1,11 @@
+// 🔐 CONTROLE DE ACESSO - @dogz_criativa
+// Para adicionar novos clientes, basta colocar a senha entre aspas e separar por vírgula.
+
+const vips = [
+    "genilton_music",
+    "loja2024",
+    "cliente_premium",
+    "123",
+    "novocliente2026",
+    
+];
